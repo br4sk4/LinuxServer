7df915cf7929466ebe27e37416e28702146a2f02
br4ska <sven.steffan@gmail.com>
2017-10-22 13:23:50 +0200

    - Corrected Errors in Daemon-Installer-Scripts

--- ShellScripts/docker-container/mongo-db/install.sh
+++ ShellScripts/docker-container/mongo-db/install.sh
@@ -4,7 +4,7 @@ docker pull mongo:3.4
 docker run --name mongo-db -dip 27017:27017 mongo
 docker stop mongo-db
 
-cp /srv/nevsoft/setup/ShellScripts/docker-container/mongo-db /etc/init.d/mongo-db
+cp /srv/nevsoft/setup/ShellScripts/docker-container/mongo-db/mongo-db /etc/init.d/mongo-db
 chown root:root /etc/init.d/mongo-db
 chmod 755 /etc/init.d/mongo-db
 
