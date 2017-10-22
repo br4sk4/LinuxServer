5d28d814b36ff542c9739aeb9c2d819d8d224ca2
br4ska <sven.steffan@gmail.com>
2017-10-22 13:32:59 +0200

    - Added MongoExpress-Daemon to Linux-Server-Image

---
+++ ShellScripts/docker-container/mongo-express/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+docker pull mongo-express:latest
+docker run --name mongoexpress --link mongo-db:mongo -dip 8081:8081 mongo-express
+docker stop mongo-express
+
+cp /srv/nevsoft/setup/ShellScripts/docker-container/mongo-express/mongo-express /etc/init.d/mongo-express
+chown root:root /etc/init.d/mongo-express
+chmod 755 /etc/init.d/mongo-express
+
+cp /srv/nevsoft/setup/ShellScripts/docker-container/mongo-express/mongo-express.service /etc/systemd/system/mongo-express.service
+chown root:root /etc/systemd/system/mongo-express.service
+chmod 755 /etc/systemd/system/mongo-express.service
+
+update-rc.d mongo-express defaults
+
+systemctl daemon-reload
+systemctl start mongo-express.service
+systemctl enable mongo-express.service
