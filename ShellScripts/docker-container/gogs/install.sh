7cc7457b08107df611b26c2aff436f4e9f65462a
br4ska <sven.steffan@gmail.com>
2017-12-02 19:39:12 +0100

    - added Gogs-Docker-Service to the server

---
+++ ShellScripts/docker-container/gogs/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p /srv/gogs
+chown root:root /srv/gogs
+chmod 755 /srv/gogs
+
+docker pull gogs/gogs:latest
+docker run --name=gogs -di -p 10022:22 -p 10080:8082 -v /srv/gogs:/data gogs/gogs
+docker stop gogs
+
+cp /srv/nevsoft/setup/ShellScripts/docker-container/gogs/gogs /etc/init.d/gogs
+chown root:root /etc/init.d/gogs
+chmod 755 /etc/init.d/gogs
+
+cp /srv/nevsoft/setup/ShellScripts/docker-container/gogs/gogs.service /etc/systemd/system/gogs.service
+chown root:root /etc/systemd/system/gogs.service
+chmod 755 /etc/systemd/system/gogs.service
+
+update-rc.d gogs defaults
+
+systemctl daemon-reload
+systemctl start gogs.service
+systemctl enable gogs.service
