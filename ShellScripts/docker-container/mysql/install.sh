3c19e987c647b441033ba7c96eb80d0c6cb742b1
br4ska <sven.steffan@gmail.com>
2017-12-02 21:02:55 +0100

    - added MySQL DockerContainer

---
+++ ShellScripts/docker-container/mysql/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p /srv/mysql
+chown root:root /srv/mysql
+chmod 755 /srv/mysql
+
+docker pull mysql/mysql:latest
+docker run --name=mysql -dip 3306:3306 -e MYSQL_ROOT_PASSWORD=mysql mysql
+docker stop mysql
+
+cp /srv/nevsoft/setup/ShellScripts/docker-container/mysql/mysql /etc/init.d/mysql
+chown root:root /etc/init.d/mysql
+chmod 755 /etc/init.d/mysql
+
+cp /srv/nevsoft/setup/ShellScripts/docker-container/mysql/mysql.service /etc/systemd/system/mysql.service
+chown root:root /etc/systemd/system/mysql.service
+chmod 755 /etc/systemd/system/mysql.service
+
+update-rc.d mysql defaults
+
+systemctl daemon-reload
+systemctl start mysql.service
+systemctl enable mysql.service
