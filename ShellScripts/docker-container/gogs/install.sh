9f161e06266f8a6f12e92c3cd9de3eaca7602bae
br4ska <sven.steffan@gmail.com>
2017-12-02 19:48:35 +0100

    - bugfix in gogs install script

--- ShellScripts/docker-container/gogs/install.sh
+++ ShellScripts/docker-container/gogs/install.sh
@@ -5,10 +5,10 @@ chown root:root /srv/gogs
 chmod 755 /srv/gogs
 
 docker pull gogs/gogs:latest
-docker run --name=gogs -di -p 10022:22 -p 10080:8082 -v /srv/gogs:/data gogs/gogs
+docker run --name=gogs -dip 10080:8082 -v /srv/gogs:/data gogs/gogs
 docker stop gogs
 
-cp /srv/nevsoft/setup/ShellScripts/docker-container/gogs/gogs /etc/init.d/gogs
+cp /srv/nevsoft/setup/ShellScripts/docker-container/gogs/ /etc/init.d/gogs
 chown root:root /etc/init.d/gogs
 chmod 755 /etc/init.d/gogs
 
